b9b4958767e9b377b5e2dcfff498bd249dff2edc
Ahmed Bally <ahmedbaly87@gmail.com>
2022-03-14 02:04:42 +0200

    test forgot password feature

---
+++ tests/Feature/Http/Controllers/Api/V1/Auth/ForgotPasswordControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Api\V1\Auth\ForgotPasswordController;
+use App\Models\User;
+use Tests\TestCase;
+
+/**
+ * Class ForgotPasswordControllerTest.
+ *
+ * @covers \App\Http\Controllers\Api\V1\Auth\ForgotPasswordController
+ */
+class ForgotPasswordControllerTest extends TestCase
+{
+    public function test__invoke(): void
+    {
+        $user = User::factory()->create();
+
+        $this->postJson(route('user.forgot-password'),
+        [
+            'email' => $user->email,
+        ])
+            ->assertStatus(200)
+            ->assertJsonStructure(['data'=>['reset_token']]);
+    }
+
+    public function test_validation(): void
+    {
+        $this->postJson(route('user.forgot-password'))
+            ->assertStatus(422)
+            ->assertJsonValidationErrors('email','errors');
+    }
+}
